76c2611ca7a3241075c53b958eed4f239650e334
Nicolas-62 <droffig@hotmail.com>
2018-04-25 14:49:18 +0200

    modif mail de confirmation/redirection membre connecté/destruction session

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -13,112 +13,152 @@ class User extends CI_Controller {
 
 	public function signup()
 	{	
-        $data['title'] = 'Identification';
-        $data['messages'] = FALSE;
-
-        $this->form_validation->set_rules('pseudo', 'pseudo', 'trim|required|is_unique[users.pseudo]',
-            array('required' => 'Vous devez renseigner un %s.',
-                    'is_unique' => 'Ce %s est déjà utilisé.', 
-                    )
-        );
-        $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email|is_unique[users.email]', 
-            array('required' => 'Vous devez renseigner une adresse mail valide.',
-                  'is_unique' => 'Cette adresse mail est déjà utilisée', 
-                    )
-        );
-        $this->form_validation->set_rules('password', 'mot de passe', 'required', 
-                array('required' => 'Vous devez renseigner un %s.')
-        );
-        if ($this->form_validation->run() === FALSE)
+        if(isset($_SESSION['pseudo']))
         {
-            $this->load->view('templates/header', $data);
-            $this->load->view('user/signup', $data);
-            $this->load->view('templates/footer');                                              
+            redirect('news');
         }
         else
         {
-            // enregistrement utilisateur
-            $pseudo = $this->user_model->signup();
-            // fin enregistrement utilisateur
+            $data['title'] = 'Identification';
+            $data['messages'] = FALSE;
 
-            // envoi mail à l'utilisateur
-            $this->email->from('nicolas.lourdel@nlmonsite.ovh', 'Nicolas Lourdel');
-            $this->email->to($this->input->post('email'));
-
-            $this->email->subject('Inscription');
-            $this->email->message('Votre inscription sur le site a bien été enregistrée</br>
-                                    Votre pseudo est : '.$pseudo);
-
-            $CR_Mail = TRUE;
-            $CR_Mail = $this->email->send();
-            $this->email->print_debugger();
-            // fin envoi mail à l'utilisateur
-            
-            if ($CR_Mail === FALSE)
+            $this->form_validation->set_rules('pseudo', 'pseudo', 'trim|required|is_unique[users.pseudo]',
+                array('required' => 'Vous devez renseigner un %s.',
+                        'is_unique' => 'Ce %s est déjà utilisé.', 
+                        )
+            );
+            $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email|is_unique[users.email]', 
+                array('required' => 'Vous devez renseigner une adresse mail valide.',
+                      'is_unique' => 'Cette adresse mail est déjà utilisée', 
+                        )
+            );
+            $this->form_validation->set_rules('password', 'mot de passe', 'required', 
+                    array('required' => 'Vous devez renseigner un %s.')
+            );
+            if ($this->form_validation->run() === FALSE)
             {
-                $message = " ### CR_Mail=$CR_Mail - Erreur envoi mail <br> \n";
+                $this->load->view('templates/header', $data);
+                $this->load->view('user/signup', $data);
+                $this->load->view('templates/footer');                                              
             }
             else
             {
-                $message = "Un mail de confirmation vous a été envoyé.";
-            }  
-            $this->session->set_userdata(array('message' => $message,
-                                                'pseudo' => $pseudo));
-            redirect('user/logged');
+                // enregistrement utilisateur
+                $pseudo = $this->user_model->signup();
+                // fin enregistrement utilisateur
+
+                // envoi mail à l'utilisateur
+                $this->email->from('nicolas.lourdel@nlmonsite.ovh', 'Nicolas Lourdel');
+                $this->email->to($this->input->post('email'));
+                // $this->email->cc('nicolas.lourdel@laposte.net');
+
+                $this->email->subject('Inscription');
+                $this->email->set_alt_message('Votre inscription sur le site a bien été enregistrée Votre pseudo est : '.$pseudo.'');
+                
+                // $this->email->attach('https://www.nlmonsite.ovh/assets/images/dessin.svg', 'attachment', 'dessin.svg');
+                $filename = 'https://www.nlmonsite.ovh/assets/images/blog.jpg';
+                $this->email->attach($filename);
+                $cid = $this->email->attachment_cid($filename);
+                $this->email->message('<p>Votre inscription sur le site a bien été enregistrée !</br>
+                                        Votre pseudo est : '.$pseudo.'</br></p>
+                                        <p>Pour retourner sur le blog cliquez ici :</p> 
+                                        <p><a href="https://www.nlmonsite.ovh/news">
+                                        <img src="cid:'. $cid .'" alt="blog de nico" />
+                                        </a></p>');
+                $CR_Mail = TRUE;
+                $CR_Mail = $this->email->send();
+                $this->email->print_debugger();
+                // fin envoi mail à l'utilisateur
+                
+                if ($CR_Mail === FALSE)
+                {
+                    $message = " Erreur envoi mail, votre inscription a bien été effectuée mais le mail de confirmation n'a pu être envoyé<br> \n";
+                }
+                else
+                {
+                    $message = "Un mail de confirmation vous a été envoyé.";
+                }  
+                $this->session->set_userdata(array('message' => $message,
+                                                    'pseudo' => $pseudo));
+                redirect('user/logged');
+            }
+        
         }
     }
     public function login()
     {
-        $data['title'] = 'Connexion';
-        $data['messages'] = FALSE;
-
-        $this->form_validation->set_rules('pseudo', 'pseudo', 'trim|required',
-                array('required' => 'Vous devez renseigner un %s.')
-        );
-        $this->form_validation->set_rules('password', 'mot de passe', 'required', 
-                array('required' => 'Vous devez renseigner un %s.')
-        );
-        if ($this->form_validation->run())
+        if(isset($_SESSION['pseudo']))
         {
-            $pseudo = $this->input->post('pseudo');
-            $password = $this->input->post('password');
-            $check_password = $this->user_model->check_id($pseudo, $password);
-
-            if($check_password === FALSE)
-            {
-                    $data['messages'] = "Vos identifiants sont incorrect."; 
-
-                $this->load->view('templates/header', $data);
-                $this->load->view('user/login', $data);
-                $this->load->view('templates/footer');  
-            }
-            else
-            {                            
-                $message = "Content de te revoir parmis nous !";
-                $this->session->set_userdata(array('message' => $message,
-                                                    'pseudo' => $pseudo));
-                redirect('user/logged'); 
-            }                      
+            redirect('news');
         }
         else
         {
-            $this->load->view('templates/header', $data);
-            $this->load->view('user/login', $data);
-            $this->load->view('templates/footer'); 
+            $data['title'] = 'Connexion';
+            $data['messages'] = FALSE;
+
+            $this->form_validation->set_rules('pseudo', 'pseudo', 'trim|required',
+                    array('required' => 'Vous devez renseigner un %s.')
+            );
+            $this->form_validation->set_rules('password', 'mot de passe', 'required', 
+                    array('required' => 'Vous devez renseigner un %s.')
+            );
+            if ($this->form_validation->run())
+            {
+                $pseudo = $this->input->post('pseudo');
+                $password = $this->input->post('password');
+                $check_password = $this->user_model->check_id($pseudo, $password);
+
+                if($check_password === FALSE)
+                {
+                        $data['messages'] = "Vos identifiants sont incorrect."; 
+
+                    $this->load->view('templates/header', $data);
+                    $this->load->view('user/login', $data);
+                    $this->load->view('templates/footer');  
+                }
+                else
+                {                            
+                    $message = "Content de te revoir parmis nous !";
+                    $this->session->set_userdata(array('message' => $message,
+                                                        'pseudo' => $pseudo));
+                    redirect('user/logged'); 
+                }                      
+            }
+            else
+            {
+                $this->load->view('templates/header', $data);
+                $this->load->view('user/login', $data);
+                $this->load->view('templates/footer'); 
+            }
         }
     }
     public function logged()
     { 
-        $data['title'] = 'Bienvenue';
+        if(isset($_SESSION['pseudo']))
+        {
+            redirect('news');
+        }
+        else
+        {
+            $data['title'] = 'Bienvenue';
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('user/logged');
-        $this->load->view('templates/footer'); 
+            $this->load->view('templates/header', $data);
+            $this->load->view('user/logged');
+            $this->load->view('templates/footer'); 
+        }
     }
 
 	public function logout()
 	{
-		$this->session->sess_destroy();
-		redirect('news');
+        if(isset($_SESSION['pseudo']))
+        {
+            redirect('news');
+        }
+        else
+        {
+            $this->session->unset_userdata('pseudo');
+    		$this->session->sess_destroy();
+    		redirect('news');
+        }
 	}
 }
